9dd7b7adca751776bd42a03631efa5cbd9b39ac0
Arthur Binda <aba@icomp.ufam.edu.br>
2019-04-24 09:35:21 -0400

    JS3 inserido

---
+++ js3/script.js
@@ -0,0 +1,10 @@
+var counter = function (init){
+    return function(){
+        return ++init;
+    }
+}
+
+var incrementar = counter(1);
+console.log("Primeira chamada " + incrementar());
+console.log("Segunda chamada " + incrementar());
+console.log("Terceira chamada " + incrementar());
